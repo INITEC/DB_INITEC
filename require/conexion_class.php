205d2c69cb41b7db17bb58ecf9dce5767c2a3bfa
Gherard <gachipana@gmail.com>
2017-04-11 10:21:59 -0500

    Cambio de Base de Datos

--- require/conexion_class.php
+++ require/conexion_class.php
@@ -15,7 +15,7 @@ class conexion {
     */	
 	//Digital Ocean
     
-    $this->_conexion = mysqli_connect("localhost", "gherard","b793RhqzWZf6jxKc","redwe_12350066_initec");
+    $this->_conexion = mysqli_connect("45.55.150.245", "gherard","b793RhqzWZf6jxKc","redwe_12350066_initec");
 
 	
 	// Server Redwebmaster
